7ec588b7ec9d1fcadf5aa8ad2d032753eb468b98
Lan Ly <lanly-dev@users.noreply.github.com>
2025-08-06 22:47:17 -0700

    feat: enhance TreeviewProvider with file decoration

--- src/treeview.ts
+++ src/treeview.ts
@@ -1,26 +1,34 @@
 import { Entry } from './interfaces'
-import { Event, EventEmitter, ThemeIcon, TreeDataProvider, TreeItem, window } from 'vscode'
+import {
+  EventEmitter, FileDecoration, FileDecorationProvider, ProviderResult, ThemeColor, ThemeIcon, TreeDataProvider,
+  TreeItem, Uri, window
+} from 'vscode'
 import Lmptm from './lmptm'
 import Browser from './browser/browser'
 
 export default class TreeviewProvider implements TreeDataProvider<Entry> {
-  public static tvProvider: TreeviewProvider
+  private static tvProvider: TreeviewProvider
+  private static fdProvider: LmptmDecorationProvider
 
-  private _onDidChangeTreeData: EventEmitter<null> = new EventEmitter<null>()
-  readonly onDidChangeTreeData: Event<null> = this._onDidChangeTreeData.event
+  private _onDidChangeTreeData = new EventEmitter<null>()
+  readonly onDidChangeTreeData = this._onDidChangeTreeData.event
 
   private browser: Browser | undefined
 
   public static create() {
+    const fileDecorationProvider = new LmptmDecorationProvider()
     const treeDataProvider = new TreeviewProvider()
     window.createTreeView(`LMPTM`, { treeDataProvider })
     // const tv = window.createTreeView('LMPTM', { treeDataProvider })
     // tv.onDidChangeSelection(({ selection }) => {})
+    window.registerFileDecorationProvider(fileDecorationProvider)
     this.tvProvider = treeDataProvider
+    this.fdProvider = fileDecorationProvider
   }
 
   public static refresh() {
     this.tvProvider.refresh()
+    this.fdProvider.refresh()
   }
 
   constructor() {
@@ -51,9 +59,40 @@ class TabItem extends TreeItem {
   constructor(e: Entry) {
     const { picked, state } = e
     let title = e.title
-    if (picked) title = `⛏️ ${title}`
     super(title)
-    if (state !== `none`) this.iconPath = new ThemeIcon(state === `playing` ? `debug-pause` : `play`)
+    this.tooltip = title
+    if (state !== `none`) {
+      this.iconPath = new ThemeIcon(state === `playing` ? `debug-pause` : `play`)
+      let uri = `lmptm://${state}`
+      if (picked) uri = `${uri}/picked`
+      if (state === `playing`) uri = `${uri}/playing`
+      else if (state === `paused`) uri = `${uri}/paused`
+      this.resourceUri = Uri.parse(uri)
+    }
     this.command = { title: `click`, command: `lmptm.click`, arguments: [e] }
+  }
+}
+
+class LmptmDecorationProvider implements FileDecorationProvider {
+  private _emitter = new EventEmitter<undefined>()
+  readonly onDidChangeFileDecorations = this._emitter.event
+
+  provideFileDecoration(uri: Uri): ProviderResult<FileDecoration> {
+    if (!uri.scheme.startsWith(`lmptm`)) return
+    let badge, tooltip, color
+    if (uri.path.includes(`picked`)) badge = '⛏️'
+    if (uri.path.includes(`playing`)) {
+      tooltip = 'Playing'
+      color = new ThemeColor('charts.green')
+    }
+    if (uri.path.includes(`paused`)) {
+      tooltip = 'Paused'
+      color = new ThemeColor('charts.yellow')
+    }
+    return { badge, color, tooltip }
+  }
+
+  refresh() {
+    this._emitter.fire(undefined)
   }
 }
